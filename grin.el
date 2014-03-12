82c32ef15d2d56cd38ed6e5d6e82352e88b402dc
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-03-12 12:55:30 +0200

    Initial config

---
+++ grin.el
@@ -0,0 +1,88 @@
+;;; grin.el --- Use grin where you might usually use grep.
+
+;; Copyright (C) 2008 Philip Jackson
+
+;; Author: Philip Jackson <phil@shellarchive.co.uk>
+;; Version: 0.4
+
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; grin.el provides a simple compilation mode for the perl grep-a-like
+;; ack (http://petdance.com/ack/).
+
+;; If `ack-guess-type' is non-nil and `ack-mode-type-map' has a
+;; reasonable value then ack.el will try and guess what you would like
+;; in the --type argument for ack.
+
+;; To install/use put ack.el in your load-path and (require 'ack) in
+;; your initialisation file. You can then M-x ack and you're off.
+
+(require 'compile)
+(require 'grep)
+
+(defvar grin-guess-type nil
+  "Setting this value to `t' will have `ack' do its best to fill
+in the --type argument to the ack command")
+
+(defvar grn-command "ack --nocolor --nogroup "
+  "The command to be run by the ack function.")
+
+(defvar grin-mode-type-map
+  '(((c++-mode) . "cpp")
+    ((c-mode) . "cc")
+    ((css-mode) . "css")
+    ((emacs-lisp-mode) . "elisp")
+    ((fortran-mode) . "fortran")
+    ((html-mode) . "html")
+    ((xml-mode nxml-mode) . "xml")
+    ((java-mode) . "java")
+    ((lisp-mode) . "lisp")
+    ((perl-mode cperl-mode) . "perl"))
+  "alist describing how to fill in the '--type=' argument to ack")
+
+(defun grin-find-type-for-mode ()
+  (catch 'found
+    (dolist (mode-type grin-mode-type-map)
+      (when (member major-mode (car mode-type))
+        (throw 'found (cdr mode-type))))))
+
+(defun grin-build-command ()
+  (let ((type (grin-find-type-for-mode)))
+    (concat grin-command
+            (when (and grin-guess-type type)
+              (concat " --type=" type)) " -- ")))
+
+(define-compilation-mode grin-mode "Grin"
+  "Ack compilation mode."
+  (set (make-local-variable 'compilation-disable-input) t)
+  (set (make-local-variable 'compilation-error-face)
+       grep-hit-face))
+
+;;;###autoload
+(defun grin (command-args)
+  (interactive
+   (list (read-from-minibuffer "Run grin (like this): "
+                               (grin-build-command)
+                               nil
+                               nil
+                               'grin-history)))
+   (compilation-start command-args 'grin-mode))
+
+(provide 'grin)
---
+++ init.el
@@ -0,0 +1,233 @@
+(server-start)
+
+(add-to-list 'load-path "~/.emacs.d/")
+
+(setq inhibit-splash-screen t) ;; no splash screen
+(setq column-number-mode t)
+
+(setq grep-highlight-matches t)
+
+(setq tab-width 4
+      whitespace-style '(face tabs spaces trailing lines space-before-tab newline indentation empty space-after-tab)
+      whitespace-line-column 80)
+
+(setq c-basic-offset 4)
+
+(setq global-whitespace-mode t)
+; space-mark tab-mark newline-mark
+
+;(setq tab-width 5
+;      whitespace-style '(trailing lines-tail space-after-tab space-before-tab)
+;      whitespace-line-column 80)
+
+(progn
+  (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))  ;; no toolbar
+  (menu-bar-mode nil) ;;no menubar
+  (scroll-bar-mode nil) ;; no scroll bar
+  )
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 4)
+ '(column-number-mode t)
+ '(default-input-method "russian-computer")
+ '(delete-old-versions t)
+ '(frame-background-mode (quote dark))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
+ '(kill-do-not-save-duplicates t)
+ '(kill-ring-max 10000)
+ '(kill-whole-line t)
+ '(make-backup-files nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
+ '(pdb-path (quote /usr/lib/python3\.2/pdb\.py))
+ '(python-honour-comment-indentation t)
+ '(python-mode-hook (quote (myyy-python-hook (lambda nil (load abbrev-file-name nil t)) (lambda nil (when py-smart-indentation (if (bobp) (save-excursion (save-restriction (widen) (while (and (not (eobp)) (or (let ((erg (syntax-ppss))) (or (nth 1 erg) (nth 8 erg))) (eq 0 (current-indentation)))) (forward-line 1)) (back-to-indentation) (py-guess-indent-offset))) (py-guess-indent-offset)))) (lambda nil (setq indent-tabs-mode py-indent-tabs-mode) (set (make-local-variable (quote beginning-of-defun-function)) (quote py-beginning-of-def-or-class)) (set (make-local-variable (quote end-of-defun-function)) (quote py-end-of-def-or-class))))))
+ '(rst-level-face-base-color "grey60")
+ '(safe-local-variable-values (quote ((test-case-name . twisted\.trial\.test) (test-case-name . twisted\.test\.test_internet\,twisted\.internet\.test\.test_posixbase) (test-case-name . twisted\.test\.test_ssl) (test-case-name . twisted\.test\.test_stdio) (test-case-name . twisted\.test\.test_fdesc) (encoding . utf-8) (test-case-name . twisted) (test-case-name . twisted\.test\.test_failure) (test-case-name . twisted\.test\.test_defer\,twisted\.test\.test_defgen\,twisted\.internet\.test\.test_inlinecb) (test-case-name . twisted\.test\.test_process) (test-case-name . twisted\.test\.test_iutils) (test-case-name . twisted\.words\.test\.test_jabbercomponent) (test-case-name . twisted\.words\.test\.test_jabberclient) (test-case-name . twisted\.test\.test_abstract) (test-case-name . twisted\.internet\.test\.test_pollingfile) (test-case-name . twisted\.test\.test_task\,twisted\.test\.test_cooperator) (test-case-name . twisted\.internet\.test\.test_inotify) (test-case-name . twisted\.test\.test_internet) (test-case-name . twisted\.web\.test\.test_xmlrpc))))
+ '(save-interprogram-paste-before-kill t)
+ '(scroll-step 1)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(x-select-enable-clipboard t)
+ '(x-select-enable-primary nil))
+
+
+
+;(setq show-trailing-whitespace t)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+;; --------------------------- tango -------------------------
+(require 'color-theme)
+(setq color-theme-load-all-themes nil)
+
+(add-to-list 'load-path "~/.emacs.d/color-theme-tangotango")
+(require 'color-theme-tangotango)
+
+;; select theme - first list element is for windowing system, second is for console/terminal
+;; Source : http://www.emacswiki.org/emacs/ColorTheme#toc9
+(setq color-theme-choices 
+      '(color-theme-tangotango color-theme-tangotango))
+
+;; default-start
+(funcall (lambda (cols)
+    	   (let ((color-theme-is-global nil))
+    	     (eval 
+    	      (append '(if (window-system))
+    		      (mapcar (lambda (x) (cons x nil)) 
+    			      cols)))))
+    	 color-theme-choices)
+
+;; test for each additional frame or console
+(require 'cl)
+(fset 'test-win-sys 
+      (funcall (lambda (cols)
+    		 (lexical-let ((cols cols))
+    		   (lambda (frame)
+    		     (let ((color-theme-is-global nil))
+		       ;; must be current for local ctheme
+		       (select-frame frame)
+		       ;; test winsystem
+		       (eval 
+			(append '(if (window-system frame)) 
+				(mapcar (lambda (x) (cons x nil)) 
+					cols)))))))
+    	       color-theme-choices ))
+;; hook on after-make-frame-functions
+(add-hook 'after-make-frame-functions 'test-win-sys)
+
+(color-theme-tangotango)
+;;
+;(if window-system
+;    (color-theme-tangotango))
+;;(if (not (window-system))
+;;    (color-theme-tty-dark))
+;; ------------------------- end tango ----------------------------------
+
+;;(require `ido)
+;;(ido-mode t)
+;;(setq ido-enable-flex-matching t)
+;;(require `uniquify)
+(autoload 'ffap "fflap" "" t)
+
+;(require 'org-install)
+(autoload 'org-install "org-install" "" t)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(autoload 'ack "ack" "" t)
+;; projects root
+(require 'project-root)
+(setq project-roots
+      `(("Django project"
+         :root-contains-files ("manage.py")
+         :filename-regex ,(regexify-ext-list '(py html css js sh))
+         :exclude-paths '("contrib"))
+        ("Sphinx documentation"
+         :root-contains-files ("Makefile" "conf.py")
+         :filename-regex ,(regexify-ext-list '(py rst))
+         :exclude-paths '("_build"))
+        ("Python project with buildout"
+         :root-contains-files ("../../buildout.cfg")
+         :filename-regex ,(regexify-ext-list '(py)))
+        ("Generic Python project"
+         :root-contains-files ("setup.py")
+         :filename-regex ,(regexify-ext-list '(py)))
+        ("Generic Mercurial project"
+         :root-contains-files (".hg"))
+        ("Generic Bazaar project"
+         :root-contains-files (".bzr"))
+        ;("Generic Subversion project"
+        ; :root-contains-files (".bzr"))
+        ("Generic git project"
+         :root-contains-files (".git"))))
+
+(global-set-key (kbd "C-c p f") 'project-root-find-file)
+(global-set-key (kbd "C-c p g") 'project-root-grep)
+(global-set-key (kbd "C-c p a") 'project-root-ack)
+(global-set-key (kbd "C-c p d") 'project-root-goto-root)
+(global-set-key (kbd "C-c p l") 'project-root-browse-seen-projects)
+
+(require 'flyspell)
+(require 'ispell)
+
+(setq ispell-program-name "aspell")
+
+(setq py-install-directory "~/.emacs.d/python-mode")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+(setq py-shell-name "ipython3")
+(setq py-load-pymacs-p t)
+;(require 'auto-complete-config)
+;(ac-config-default)
+
+;whitespace-mode
+(defun myyy-python-hook ()
+  ;(setq tab-width 4
+  ;      py-indent-offset 4
+  ;      indent-tabs-mode nil
+  ;      py-smart-indentation t)
+  (require 'whitespace)
+  (whitespace-mode t))
+
+(add-hook 'python-mode-hook 'myyy-python-hook)
+
+(defun myyy-rst-hook ()
+  (setq tab-width 4
+        indent-tabs-mode nil)
+  (require 'whitespace)
+  (whitespace-mode t))
+
+(add-hook 'rst-mode-hook 'myyy-rst-hook)
+
+(add-to-list 'load-path "~/.emacs.d/coffee-mode")
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun myyy-coffee-hook ()
+  (setq tab-width 4
+        indent-tabs-mode nil)
+  (require 'whitespace)
+  (whitespace-mode t))
+
+(add-hook 'coffee-mode-hook 'myyy-coffee-hook)
+
+(require 'actionscript-mode)
+(add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
+
+(defun myyy-actionscript-hook ()
+  (setq tab-width 4
+        indent-tabs-mode nil)
+  (require 'whitespace)
+  (whitespace-mode t))
+
+(add-hook 'actionscript-mode-hook 'myyy-actionscript-hook)
+
+(require 'cython-mode)
+(add-to-list 'auto-mode-alist '("\\.pyx$" . cython-mode))
+
+(defun myyy-cython-hook ()
+  (setq tab-width 4
+        indent-tabs-mode nil)
+  (require 'whitespace)
+  (whitespace-mode t))
+
+(add-hook 'cython-mode-hook 'myyy-cython-hook)
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 113 :family "DejaVu Sans Mono")))))
+
